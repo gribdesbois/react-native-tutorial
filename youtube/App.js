bd4c4904595d08d16c8bef04b33980b18b7b95cc
gribdesbois <gribdesbois@gmail.com>
2022-02-17 02:20:33 +0100

    bit of cleaning

--- youtube/App.js
+++ youtube/App.js
@@ -22,9 +22,7 @@ class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text>Still working!</Text>
       </View>
     )
   }
@@ -38,16 +36,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 })
 
